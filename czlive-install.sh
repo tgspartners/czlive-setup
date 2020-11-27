673f94c966a576d2b46820614e9a66f34a1d11ab
TJ Sheppard <7302775+tgspartners@users.noreply.github.com>
2020-11-27 02:16:19 +0000

    Create czlive-install.sh

---
+++ czlive-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /
+mkdir /czlive
+cd /czlive
+wget https://downloads.sourceforge.net/project/clonezilla/clonezilla_live_stable/2.7.0-10/clonezilla-live-2.7.0-10-amd64.iso
+cd /
+ls /czlive* --recursive
