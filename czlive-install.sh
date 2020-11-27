64f062940b6839dd6c3ebf07df85dbc399da2e77
TJ Sheppard <7302775+tgspartners@users.noreply.github.com>
2020-11-27 04:00:44 +0000

    Update czlive-install.sh

--- czlive-install.sh
+++ czlive-install.sh
@@ -2,6 +2,6 @@
 
 
 cd /czlive-setup-main
-wget --verbose --output-file=czlive.iso https://downloads.sourceforge.net/project/clonezilla/clonezilla_live_stable/2.7.0-10/clonezilla-live-2.7.0-10-amd64.iso
+wget --output-file=czlive.iso https://downloads.sourceforge.net/project/clonezilla/clonezilla_live_stable/2.7.0-10/clonezilla-live-2.7.0-10-amd64.iso
 
 
